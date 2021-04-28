14b1e2a9a54d174280928e56b65cefb3dac33eef
Drkeck <k3ckl3y.a@gmail.com>
2021-04-27 23:25:22 -0400

    added some basic server script to get the general idea started and used Create-React-App for the client side so i can reuse structures more efficently then using handlebars or other alternitives since react kinda tells you when something is off

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// Required tech.
+const express = require('express');
+const path = require('path');
+
+// Database setup.
+const db //being set up
+
+// server setup.
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// for socket.io intigration.
+var http = require('http').Server(app)
+
+// extra lines for potential session data and other such nonsense
+
+//apps
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client/public")));
+// app.use sessions
+// app.use Routes
+
+app.listen(PORT, () => console.log("Love-Its Server Ready for the sweet little notes."));
